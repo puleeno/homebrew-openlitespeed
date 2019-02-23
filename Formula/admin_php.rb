952230e81dcdb2acba96006841c3ba02ba783106
Puleeno Nguyen <puleeno@gmail.com>
2019-02-23 17:37:13 +0700

    Restore to old version

--- Formula/admin_php.rb
+++ Formula/admin_php.rb
@@ -1,139 +1,50 @@
 class AdminPhp < Formula
-  desc "General-purpose scripting language"
-  homepage "https://secure.php.net/"
-  url "https://php.net/get/php-7.2.15.tar.xz/from/this/mirror"
-  sha256 "75e90012faef700dffb29311f3d24fa25f1a5e0f70254a9b8d5c794e25e938ce"
+  homepage "https://secure.php.net"
+  url "https://secure.php.net/get/php-7.2.10.tar.xz/from/this/mirror"
+  sha256 "01c2154a3a8e3c0818acbdbc1a956832c828a0380ce6d1d14fea495ea21804f0"
 
-  keg_only :versioned_formula
-  depends_on "pkg-config" => :build
-  depends_on "autoconf"
   depends_on "expat"
   depends_on "libxml2"
   depends_on "openssl"
   depends_on "zlib"
   depends_on "libzip"
 
-  # PHP build system incorrectly links system libraries
-  # see https://github.com/php/php-src/pull/3472
-  patch :DATA
-
   def install
-    # Ensure that libxml2 will be detected correctly in older MacOS
-    if MacOS.version == :el_capitan || MacOS.version == :sierra
-      ENV["SDKROOT"] = MacOS.sdk_path
-    end
+      config_path = etc/"lsphp/admin/#{php_version}"
 
-    # buildconf required due to system library linking bug patch
-    system "./buildconf", "--force"
+      # Each extension that is built on Mojave needs a direct reference to the
+      # sdk path or it won't find the headers
+      headers_path = "=#{MacOS.sdk_path_if_needed}/usr"
+
+      # Required due to icu4c dependency
+      ENV.cxx11
+
+      args = %W[
+          --prefix=#{prefix}
+          --disable-all
+          --with-litespeed
+          --enable-zip
+          --enable-xml
+          --enable-json
+          --enable-sockets
+          --enable-session
+          --enable-posix
+          --enable-bcmath
+          --with-libzip
+          --with-bz2#{headers_path}
+          --with-zlib=#{Formula["zlib"].opt_prefix}
+          --with-openssl=#{Formula["openssl"].opt_prefix}
+          --with-sqlite3=#{Formula["sqlite"].opt_prefix}
+          --with-libexpat-dir=#{Formula["expat"].opt_prefix}
+      ]
 
 
-    # Required due to icu4c dependency
-    ENV.cxx11
-
-    config_path = etc/"admin_php/#{php_version}"
-    # Prevent system pear config from inhibiting pear install
-    (config_path/"pear.conf").delete if (config_path/"pear.conf").exist?
-
-    # Prevent homebrew from harcoding path to sed shim in phpize script
-    ENV["lt_cv_path_SED"] = "sed"
-
-    # Each extension that is built on Mojave needs a direct reference to the
-    # sdk path or it won't find the headers
-    headers_path = "=#{MacOS.sdk_path_if_needed}/usr"
-
-    args = %W[
-      --prefix=#{prefix}
-      --disable-all
-      --with-litespeed
-      --enable-zip
-      --enable-xml
-      --enable-json
-      --enable-sockets
-      --enable-session
-      --enable-posix
-      --enable-bcmath
-      --with-libzip
-      --enable-mysqlnd
-      --enable-pdo
-      --with-bz2#{headers_path}
-      --with-zlib=#{Formula["zlib"].opt_prefix}
-      --with-openssl=#{Formula["openssl"].opt_prefix}
-      --with-sqlite3=#{Formula["sqlite"].opt_prefix}
-      --with-libexpat-dir=#{Formula["expat"].opt_prefix}
-      --with-mysql-sock=/tmp/mysql.sock
-      --with-mysqli=mysqlnd
-      --with-pdo-mysql=mysqlnd
-    ]
-    system "./configure", *args
-    system "make"
-    system "make", "install"
+      system "./configure", *args
+      system "make"
+      system "make", "install"
   end
 
   def php_version
-    version.to_s.split(".")[0..1].join(".")
+      version.to_s.split(".")[0..1].join(".")
   end
 end
-
-__END__
-diff --git a/acinclude.m4 b/acinclude.m4
-index 168c465f8d..6c087d152f 100644
---- a/acinclude.m4
-+++ b/acinclude.m4
-@@ -441,7 +441,11 @@ dnl
- dnl Adds a path to linkpath/runpath (LDFLAGS)
- dnl
- AC_DEFUN([PHP_ADD_LIBPATH],[
--  if test "$1" != "/usr/$PHP_LIBDIR" && test "$1" != "/usr/lib"; then
-+  case "$1" in
-+  "/usr/$PHP_LIBDIR"|"/usr/lib"[)] ;;
-+  /Library/Developer/CommandLineTools/SDKs/*/usr/lib[)] ;;
-+  /Applications/Xcode*.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*/usr/lib[)] ;;
-+  *[)]
-     PHP_EXPAND_PATH($1, ai_p)
-     ifelse([$2],,[
-       _PHP_ADD_LIBPATH_GLOBAL([$ai_p])
-@@ -452,8 +456,8 @@ AC_DEFUN([PHP_ADD_LIBPATH],[
-       else
-         _PHP_ADD_LIBPATH_GLOBAL([$ai_p])
-       fi
--    ])
--  fi
-+    ]) ;;
-+  esac
- ])
-
- dnl
-@@ -487,7 +491,11 @@ dnl add an include path.
- dnl if before is 1, add in the beginning of INCLUDES.
- dnl
- AC_DEFUN([PHP_ADD_INCLUDE],[
--  if test "$1" != "/usr/include"; then
-+  case "$1" in
-+  "/usr/include"[)] ;;
-+  /Library/Developer/CommandLineTools/SDKs/*/usr/include[)] ;;
-+  /Applications/Xcode*.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/*/usr/include[)] ;;
-+  *[)]
-     PHP_EXPAND_PATH($1, ai_p)
-     PHP_RUN_ONCE(INCLUDEPATH, $ai_p, [
-       if test "$2"; then
-@@ -495,8 +503,8 @@ AC_DEFUN([PHP_ADD_INCLUDE],[
-       else
-         INCLUDES="$INCLUDES -I$ai_p"
-       fi
--    ])
--  fi
-+    ]) ;;
-+  esac
- ])
-
- dnl internal, don't use
-@@ -2411,7 +2419,8 @@ AC_DEFUN([PHP_SETUP_ICONV], [
-     fi
-
-     if test -f $ICONV_DIR/$PHP_LIBDIR/lib$iconv_lib_name.a ||
--       test -f $ICONV_DIR/$PHP_LIBDIR/lib$iconv_lib_name.$SHLIB_SUFFIX_NAME
-+       test -f $ICONV_DIR/$PHP_LIBDIR/lib$iconv_lib_name.$SHLIB_SUFFIX_NAME ||
-+       test -f $ICONV_DIR/$PHP_LIBDIR/lib$iconv_lib_name.tbd
-     then
-       PHP_CHECK_LIBRARY($iconv_lib_name, libiconv, [
-         found_iconv=yes
